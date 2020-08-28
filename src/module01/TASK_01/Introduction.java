9f38787a25714d895428b19e41d1c7e6dfd818fe
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-08-28 17:15:58 +0300

    module01.TASK_01.Introduction completed

---
+++ src/module01/TASK_01/Introduction.java
@@ -0,0 +1,14 @@
+package module01.TASK_01;
+
+public class Introduction {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Hello! My name is Andrew ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("I want to be a java developer!");
+        }
+    }
+}
