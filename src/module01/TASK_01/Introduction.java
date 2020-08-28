208b8d52a84caa4af3a14c5f213c79aa192115c2
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-08-28 19:45:08 +0300

    Hello word changed to Hell

--- src/module01/TASK_01/Introduction.java
+++ src/module01/TASK_01/Introduction.java
@@ -3,12 +3,13 @@ package module01.TASK_01;
 public class Introduction {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.print("Hello! My name is Andrew ");
+            System.out.print("Hell! My name is Andrew ");
         }
         System.out.println();
 
         for (int i = 0; i < 99; i++) {
             System.out.println("I want to be a java developer!");
         }
+
     }
 }
