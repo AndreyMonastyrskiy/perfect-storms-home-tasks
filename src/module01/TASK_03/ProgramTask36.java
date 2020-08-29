08b2995d57420ea7adff6724f159f09744a2318c
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-08-29 14:02:58 +0300

    task6 from lesson 3 completed

---
+++ src/module01/TASK_03/ProgramTask36.java
@@ -0,0 +1,10 @@
+package module01.TASK_03;
+
+public class ProgramTask36 {
+    public static void main(String[] args) {
+        // char type takes 16 bits of memory and can store values from 0 to 65535
+        System.out.println('+');
+        System.out.println("!");
+        System.out.println('?');
+    }
+}
