f86f8c52ecf62516fb28af9500344df53df8153d
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-08-29 17:08:40 +0300

    task1 from lesson 4 completed

---
+++ src/module01/TASK_04/ProgramTask41.java
@@ -0,0 +1,21 @@
+package module01.TASK_04;
+
+public class ProgramTask41 {
+    public static void main(String[] args) {
+        int a = 10 + 2;
+        int b = a + 5;
+        int c = (a - b);
+        c++;
+        int d = a * b * c;
+        int e = d / 8;
+        int f = e % 30;
+        int i = -f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(i);
+    }
+}
