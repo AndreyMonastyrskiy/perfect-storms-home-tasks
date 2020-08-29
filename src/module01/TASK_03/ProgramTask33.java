f56f194a5686e9c4d30d2d17d016c55998206878
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-08-29 13:08:04 +0300

    task3 from lesson 3 completed

---
+++ src/module01/TASK_03/ProgramTask33.java
@@ -0,0 +1,11 @@
+package module01.TASK_03;
+
+public class ProgramTask33 {
+    public static void main(String[] args) {
+        //int type takes 32 bits of memory and can store values from -2147483648 to 2147483647
+        int x = 1200;
+        System.out.println(x);
+        System.out.println((int) Math.pow(x, 2));
+        System.out.println((int) Math.pow(x, 3));
+    }
+}
