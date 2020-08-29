ecb223aa2f5c81551eeedeb08026628e608934eb
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-08-29 14:22:23 +0300

    task7 from lesson 3 completed

---
+++ src/module01/TASK_03/ProgramTask37.java
@@ -0,0 +1,18 @@
+package module01.TASK_03;
+
+public class ProgramTask37 {
+    public static void main(String[] args) {
+        int a = 'r';
+        char b = 123;
+        // float c = 7,2; - Error. Correct: float c = 7.2f;
+        // short d = 3L; - Error. Correct: short d = 3;
+        double e = 0x123D;
+        int f = 0x123F;
+        // int g = 078; - Error. Correct: int g = 78;, or int g = 0x78;
+        short h = 7;
+        // byte i = 255; - Error. Need to use short instead of byte;
+        double j = 9;
+        // int k = 5,5; - Error. Correct: float k = 5.5f;
+        int l = 5;
+    }
+}
