ab116b056ac5db832440fd19961e58f02db99c7b
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-08-29 14:45:39 +0300

    task8 from lesson 3 completed

---
+++ src/module01/TASK_03/ProgramTask38.java
@@ -0,0 +1,33 @@
+package module01.TASK_03;
+
+public class ProgramTask38 {
+    public static void main(String[] args) {
+        // 123/2 = 61 (1)
+        // 61/2 = 30 (1)
+        // 30/2 = 15 (0)
+        // 15/2 = 7 (1)
+        // 7/2 = 3 (1)
+        // 3/2 = 1 (1)
+        // 1/2 = 0 (1)
+
+        int num123b = 0b1111011; // binary number
+
+        // 123/8 = 15 (3)
+        // 15/8 = 1 (7)
+        // 1/8 = 0 (1)
+        int num123o = 0173; // octal number
+
+
+        // 123/16 = 7 (11)
+        // 7/16 = 0 (7)
+
+        int num123h = 0x7B; // hexadecimal number
+
+        // остаток 10 это A
+        // остаток 11 это B
+        // остаток 12 это C
+        // остаток 13 это D
+        // остаток 14 это E
+        // остаток 15 это F
+    }
+}
