f1b720698bf88b3b0bbac0b2f047466336afe1fa
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-08-29 12:53:14 +0300

    task1 from lesson 3 completed

---
+++ src/module01/TASK_03/ProgramTask31.java
@@ -0,0 +1,11 @@
+package module01.TASK_03;
+
+public class ProgramTask31 {
+    public static void main(String[] args) {
+        //Byte type takes 8 bits of memory and can store values from -128 to 127
+        byte firstValue = 127;
+        short secondValue = 255;
+        byte result = (byte) (firstValue - secondValue);
+        System.out.println("Result with byte type: " + result);
+    }
+}
