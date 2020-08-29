3debea4f0f8a006bd9b087255f7d4091613a9eef
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-08-29 13:15:41 +0300

    task4 from lesson 3 completed

---
+++ src/module01/TASK_03/ProgramTask34.java
@@ -0,0 +1,8 @@
+package module01.TASK_03;
+
+public class ProgramTask34 {
+    public static void main(String[] args) {
+        // long type is used to store very large values. For example, to store unique identifiers, timestamps, etc.
+        System.out.println("Long type maximum value: " + 9223372036854775807L);
+    }
+}
