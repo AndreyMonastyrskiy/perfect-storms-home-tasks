b211fba5e9d594ff7a489fcb8e7ecfe10791e141
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-08-30 13:29:36 +0300

    Task 5 for lesson 5 completed

---
+++ src/module01/TASK_05/ProgramTask55.java
@@ -0,0 +1,11 @@
+package module01.TASK_05;
+
+public class ProgramTask55 {
+    public static void main(String[] args) {
+        int num = 8;
+        String message = "";
+
+        message = num > 10 ? "Number is greater than 10" : "Number is less than or equal to 10";
+        System.out.println(message);
+    }
+}
