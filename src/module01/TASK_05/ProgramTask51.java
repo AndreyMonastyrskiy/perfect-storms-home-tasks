93b917e5679b049a4c73dc9045e5c321c901900f
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-08-30 12:01:53 +0300

    Task 1 for lesson 5 completed

---
+++ src/module01/TASK_05/ProgramTask51.java
@@ -0,0 +1,13 @@
+package module01.TASK_05;
+
+public class ProgramTask51 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = --a; // a = 6, b = 6
+
+        if (b == 7) { // False
+            System.out.println("b = 7");
+        }
+        // Nothing displayed...
+    }
+}
