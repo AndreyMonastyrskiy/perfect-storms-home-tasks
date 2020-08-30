bde2f3cb8c8233a2e7c07df85fe0c281fab858b8
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-08-30 20:28:50 +0300

    Updated task 2 for lesson 7

--- src/module01/TASK_07/ProgramTask72.java
+++ src/module01/TASK_07/ProgramTask72.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class ProgramTask72 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter any natural number:");
+        System.out.println("Input data:");
         long n = scanner.nextLong();
         long counter = 0;
         while (counter <= n) {
-            System.out.println(counter * counter);
+            System.out.print(counter * counter + " ");
             counter++;
         }
     }
