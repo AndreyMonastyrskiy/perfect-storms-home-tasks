4122c8160eb753bac7dfc0a9bce331ac1aa86fb2
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-08-31 20:13:22 +0300

    Completed task 2 from lesson 9

---
+++ src/module01/TASK_09/ProgramTask92.java
@@ -0,0 +1,9 @@
+package module01.TASK_09;
+
+public class ProgramTask92 {
+    public static void main(String[] args) {
+        String phrase = "London - is a capital of England";
+        String newPhrase = phrase.replace("England", "Great Britain");
+        System.out.println(newPhrase);
+    }
+}
