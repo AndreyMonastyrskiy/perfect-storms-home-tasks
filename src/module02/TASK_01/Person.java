91501be56952344667dcf2fb0175d66daca8af1c
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-09-01 12:11:46 +0300

    Completed task 1 from lesson 1 (module 2)

---
+++ src/module02/TASK_01/Person.java
@@ -0,0 +1,11 @@
+package module02.TASK_01;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/module02/TASK_01/ProgramTask11.java
@@ -0,0 +1,8 @@
+package module02.TASK_01;
+
+public class ProgramTask11 {
+    public static void main(String[] args) {
+        Person person = new Person("Sam", 33);
+        System.out.println(person.name + " " + person.age);
+    }
+}
