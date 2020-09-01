2247a2fc638380712608ed7f200120aa7c02c2ff
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-09-01 19:29:56 +0300

    Completed task 4 from lesson 3 (module 2)

---
+++ src/module02/TASK_03/ProgramTask34.java
@@ -0,0 +1,22 @@
+package module02.TASK_03;
+
+public class ProgramTask34 {
+    /*
+    1. Can an abstract class be instantiated? Answer: no.
+    2. Is it possible to define a constructor for an abstract class? Answer: yes.
+    3. Can there be a non-abstract method in an abstract class? Answer: yes.
+    4. Can there be abstract and non-abstract methods in an abstract class? Answer: yes.
+    5. Can there be an abstract method in a non-abstract class? Answer: no.
+    6. Can a non-abstract method be overridden? Answer: yes.
+    7. Are all methods of an interface abstract? Answer: no, there can be default methods.
+    8. Is it possible to create an interface without a method? Answer: yes.
+    9. What is the difference between an abstract class and an interface?
+       Answer:
+       - Interface cannot have implementations.
+       - Abstract class can have instance methods that implements a default behavior.
+       - Variables declared in a interface is by default final.
+       - Abstract class may contain non-final variables.
+       - An interface can extend another interface only.
+       - An abstract class can extend another class and implement multiple interfaces.
+    */
+}
