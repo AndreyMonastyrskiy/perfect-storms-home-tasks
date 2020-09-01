f4f89cbd3e7eeb85f82256449ecad4db2b232e30
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-09-01 12:22:20 +0300

    Completed task 2 from lesson 1 (module 2)

--- src/module02/TASK_01/Person.java
+++ src/module02/TASK_01/Person.java
@@ -1,5 +1,7 @@
 package module02.TASK_01;
 
+import java.util.Date;
+
 public class Person {
     String name;
     int age;
@@ -7,5 +9,17 @@ public class Person {
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void meditaion() {
+        System.out.println(String.format("I think - it means I exist. (%s) %s", this.name, new Date().toString()));
     }
 }
---
+++ src/module02/TASK_01/ProgramTask12.java
@@ -0,0 +1,8 @@
+package module02.TASK_01;
+
+public class ProgramTask12 {
+    public static void main(String[] args) {
+        Person person = new Person("Sam", 33);
+        person.meditaion();
+    }
+}
