cb1d3064e4f84c5784d8526bca7c0416c451aeba
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-09-02 16:27:44 +0300

    Completed task 3 from lesson 4 (module 2). Maybe need to correct later.

---
+++ src/module02/TASK_04/Leon.java
@@ -0,0 +1,13 @@
+package module02.TASK_04;
+
+public class Leon {
+    private String name;
+
+    public Leon(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ src/module02/TASK_04/ProgramTask43.java
@@ -0,0 +1,9 @@
+package module02.TASK_04;
+
+public class ProgramTask43 {
+    public static void main(String[] args) {
+        Leon leo = new Leon("Woolf");
+        System.out.println(leo.getName());
+
+    }
+}
