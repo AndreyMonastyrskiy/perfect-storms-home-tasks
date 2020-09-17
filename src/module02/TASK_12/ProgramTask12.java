5978146aa92066f5806ee7421e09cac10af4e94c
Andrey Monastyrskiy <ghjcnstxbckf@gmail.com>
2020-09-17 15:13:57 +0300

    part2 from lesson 12

--- src/module02/TASK_12/ProgramTask12.java
+++ src/module02/TASK_12/ProgramTask12.java
@@ -1,5 +1,11 @@
 package module02.TASK_12;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ProgramTask12 {
     public static void main(String[] args) {
         Record<Integer, String, String> record1 = new Record<>(0, "name1", "value1");
@@ -12,6 +18,18 @@ public class ProgramTask12 {
         record3.setValue(true);
         System.out.println(record3);
 
+        List<String> list1 = new ArrayList<>(Arrays.asList("Value1", "Value2", "Value3"));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 7, 9));
+        fillListWithDefault(list1, "Hello");
+        fillListWithDefault(list2, 47);
+        System.out.println(list1);
+        System.out.println(list2);
 
+    }
+
+    private static <T> void fillListWithDefault(List<T> list, T defaultValue) {
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, defaultValue);
+        }
     }
 }
