61eae519a94ddc84ee7847431013b31e3d3fe91f
Andrey Monastyrskiy <ghjcnstxbckf@gmail.com>
2020-09-17 14:39:58 +0300

    part1 from lesson 12 updated

--- src/module02/TASK_12/ProgramTask12.java
+++ src/module02/TASK_12/ProgramTask12.java
@@ -5,8 +5,11 @@ public class ProgramTask12 {
         Record<Integer, String, String> record1 = new Record<>(0, "name1", "value1");
         Record<String, Short, Double> record2 = new Record<>("1", (short)47, 47.0d);
         Record<String, String, Boolean> record3 = new Record<>("2", "name3", false);
+        record1.setId(-1);
         System.out.println(record1);
+        record2.setName((short)33);
         System.out.println(record2);
+        record3.setValue(true);
         System.out.println(record3);
 
 
