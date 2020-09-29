22a178f53979a65fce3bf9217f0c892620e98c7b
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-09-29 16:45:45 +0300

    Found unicode characters for chess board. Final Task module2

---
+++ src/module02/FINAL_TASK/ChessBoard.java
@@ -0,0 +1,5 @@
+package module02.FINAL_TASK;
+
+public class ChessBoard {
+
+}
---
+++ src/module02/FINAL_TASK/ChessGame.java
@@ -0,0 +1,11 @@
+package module02.FINAL_TASK;
+
+public class ChessGame {
+    public static void main(String[] args) {
+        System.out.println("|\u20051\u2006|\u20052\u2006|\u20053\u2006|\u20054\u2006|");
+        System.out.println("------------");
+        System.out.println("|\u2654|\u2001|\u2654|\u265B|");
+        System.out.println("------------");
+        System.out.println("|\u2001|\u2654|\u265E|\u2654|");
+    }
+}
