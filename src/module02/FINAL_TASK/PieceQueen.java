f45667426009acf10432aecee3d8b649d34f90f6
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-10-01 12:08:33 +0300

    Created chess piece Queen

---
+++ src/module02/FINAL_TASK/PieceQueen.java
@@ -0,0 +1,25 @@
+package module02.FINAL_TASK;
+
+public class PieceQueen implements ChessPiece{
+    private ChessPieceColor color;
+
+    public PieceQueen (ChessPieceColor color) {
+        this.color = color;
+    }
+
+    @Override
+    public String getImage() {
+        return this.color == ChessPieceColor.WHITE ? "\u2655" : "\u265A";
+    }
+
+    @Override
+    public boolean move(ChessBoard chessBoard, String from, String to) {
+        // TODO
+        return false;
+    }
+
+    @Override
+    public ChessPieceColor getColor() {
+        return this.color;
+    }
+}
