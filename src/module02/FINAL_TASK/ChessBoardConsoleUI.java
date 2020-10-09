d7409e948952676dcb824aa2f6283ba171727c14
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-10-09 16:48:08 +0300

    Added ChessBoardConsoleUI

---
+++ src/module02/FINAL_TASK/ChessBoardConsoleUI.java
@@ -0,0 +1,11 @@
+package module02.FINAL_TASK;
+
+/**
+ * The class implements the logic for displaying the state of a chessboard with figures on the console (terminal).
+ *
+ * @author Andrey Monastyrskiy
+ */
+
+public class ChessBoardConsoleUI {
+
+}
