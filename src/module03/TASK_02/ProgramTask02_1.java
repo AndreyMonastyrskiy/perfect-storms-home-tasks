b816889588dbc25c70b7ffd435da036067b35902
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-10-16 11:12:56 +0300

    Completed task1 from lesson 2 module 3

---
+++ src/module03/TASK_02/ProgramTask02_1.java
@@ -0,0 +1,60 @@
+package module03.TASK_02;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ProgramTask02_1 {
+    public static void main(String[] args) {
+        long startTime = 0;
+        long endTime = 0;
+        List<String> arrayList = new ArrayList<>();
+        List<String> linkedList = new LinkedList<>();
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            arrayList.add("Element" + i);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Time for adding elements to the end of ArrayList: " + (endTime - startTime) + " ms");
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            linkedList.add("Element" + i);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Time for adding elements to the end of LinkedList: " + (endTime - startTime) + " ms");
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            int index = arrayList.indexOf("Element" + i);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Time for searching elements in ArrayList: " + (endTime - startTime) + " ms");
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            int index = linkedList.indexOf("Element" + i);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Time for searching elements in LinkedList: " + (endTime - startTime) + " ms");
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            arrayList.remove("Element" + i);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Time for removing elements from ArrayList: " + (endTime - startTime) + " ms");
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            linkedList.remove("Element" + i);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Time for removing elements from LinkedList: " + (endTime - startTime) + " ms");
+
+        System.out.println("ArrayList work faster in search operations, " +
+                "and when it is necessary to refer to elements by index.");
+        System.out.println("LinkedList work faster when it is necessary to frequently remove or add new elements.");
+    }
+}
---
+++ src/module03/TASK_02/ProgramTask02_2.java
@@ -0,0 +1,5 @@
+package module03.TASK_02;
+
+public class ProgramTask02_2 {
+
+}
