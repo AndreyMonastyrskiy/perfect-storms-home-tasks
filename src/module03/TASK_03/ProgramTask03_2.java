1874a83228a8ff48c0eb6cce263eff8cb837bac0
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-10-21 12:44:33 +0300

    partially completed task 2 from lesson 3 of module3

--- src/module03/TASK_03/ProgramTask03_2.java
+++ src/module03/TASK_03/ProgramTask03_2.java
@@ -1,5 +1,7 @@
 package module03.TASK_03;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -8,13 +10,13 @@ public class ProgramTask03_2 {
     public static void main(String[] args) {
         System.out.println("Fast Armstrong numbers search for long number (precomputed numbers):");
         long a = System.currentTimeMillis();
-        System.out.println(Arrays.toString(fastGetNumber(Long.MAX_VALUE)));
+        System.out.println(Arrays.toString(fastGetNumbers(Long.MAX_VALUE)));
         long b = System.currentTimeMillis();
         System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (8 * 128 * 1024));
         System.out.println("time = " + (b - a) / 1000);
         System.out.println("Slow Armstrong numbers search for long number:");
         a = System.currentTimeMillis();
-        System.out.println(Arrays.toString(getNumbers(100_000_000L)));
+        System.out.println(Arrays.toString(getNumbersMap(100_000_000L)));
         b = System.currentTimeMillis();
         System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (8 * 128 * 1024));
         System.out.println("time = " + (b - a) / 1000);
@@ -42,7 +44,29 @@ public class ProgramTask03_2 {
         return result;
     }
 
-    public static long[] fastGetNumber(long number) {
+    public static long[] getNumbersMap(long N) {
+        HashMap<Integer, long[]> powers = new HashMap<>();
+        long[] result = {};
+        int nLength = String.valueOf(N).length();
+
+        // compute all powers
+        for (int i = 1; i <= nLength; i++) {
+            long[] elements = new long[10];
+            for (int j = 0; j <= 9; j++) {
+                elements[j] = (long)Math.pow(j, i);
+            }
+            powers.put(i, elements);
+        }
+        // find all armstrong numbers
+        for (long i = 1; i < N; i++) {
+            if (isArmstrongNumber(i, powers)) {
+                result = addToLongArray(result, i);
+            }
+        }
+        return result;
+    }
+
+    public static long[] fastGetNumbers(long number) {
        long[] armstrongNumbers = {1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 153L, 370L, 371L, 407L, 1634L, 8208L, 9474L,
                54748L, 92727L, 93084L, 548834L, 1741725L, 4210818L, 9800817L, 9926315L, 24678050L, 24678051L,
                88593477L, 146511208L, 472335975L, 534494836L, 912985153L, 4679307774L, 32164049650L, 32164049651L,
@@ -76,11 +100,28 @@ public class ProgramTask03_2 {
         return false;
     }
 
-    public static ArrayList<Long> getUniqueNumbers(long number) {
-        ArrayList<Long> result = new ArrayList<>();
+    private static boolean isArmstrongNumberMap(long number, HashMap<Integer, long[]> powers) {
+        ArrayList<Long> longDigits = new ArrayList<>();
+        BigInteger sum = BigInteger.valueOf(0);
 
-        return result;
+        // get separate numbers from input
+        long temp = number;
+        do {
+            longDigits.add(0,temp % 10);
+            temp /= 10;
+        } while (temp > 0);
+
+        // compute sum
+        for (Long digit: longDigits) {
+            sum = sum.add(BigInteger.valueOf(powers.get(longDigits.size())[digit.intValue()]));
+        }
+        // check number
+        if (sum.compareTo(BigInteger.valueOf(number)) == 0) {
+            return true;
+        }
+        return false;
     }
+
 
     private static long[] addToLongArray(long[] array, long item) {
         long[] result = new long[array.length + 1];
