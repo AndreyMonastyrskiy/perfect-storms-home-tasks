a1e73dd7f676382d7a3601ef374a8f89610fb966
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-10-28 17:57:45 +0300

    Completed task3 from lesson5 module3

---
+++ src/module03/TASK_05/ProgramTask05_03.java
@@ -0,0 +1,26 @@
+package module03.TASK_05;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class ProgramTask05_03 {
+    public static void main(String[] args) {
+        List<Object> objects = new ArrayList<>();
+        String obj1 = new String("Object1");
+        String obj2 = new String("Object2");
+        String obj3 = new String("Object3");
+        objects.add(obj1);
+        objects.add(obj2);
+        objects.add(obj2);
+        objects.add(obj3);
+        objects.add(obj3);
+        objects.add(obj3);
+        System.out.println(objects);
+        System.out.println(getUniqueObjects(objects));
+    }
+
+    public static List<Object> getUniqueObjects(List<Object> inputList) {
+        return new ArrayList<>(new HashSet<>(inputList));
+    }
+}
